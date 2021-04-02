e0d0b2840919522ea3c8f4781298dcf04abcd3ce
LaggingNinja <66457844+LaggingNinja@users.noreply.github.com>
2021-04-02 19:21:02 +0800

    (2/4/2021 NEWEST) -finished database connect of booking_books.java booking_computers.java, booking_rooms.java

--- app/src/main/java/com/example/collegeappv02/booking_books.java
+++ app/src/main/java/com/example/collegeappv02/booking_books.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class booking_books extends AppCompatActivity {
 
-    Button button;
+    TextInputEditText textInputEditTextStudentID, textInputEditTextBookID, textInputEditTextBorrowDate, textInputEditTextReturnDate;
+    Button button, apply;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,65 @@ public class booking_books extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Booking.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+        textInputEditTextStudentID = findViewById(R.id.booking_books_student_id);
+        textInputEditTextBookID = findViewById(R.id.booking_books_book_id);
+        textInputEditTextBorrowDate = findViewById(R.id.booking_books_borrowdate);
+        textInputEditTextReturnDate = findViewById(R.id.booking_books_returndate);
+        apply = findViewById(R.id.buttonApplyBooks);
+        progressBar = findViewById(R.id.progress);
+
+
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String student_id, book_id, borrowdate, returndate;
+                student_id = String.valueOf(textInputEditTextStudentID.getText());
+                book_id = String.valueOf(textInputEditTextBookID.getText());
+                borrowdate = String.valueOf(textInputEditTextBorrowDate.getText());
+                returndate = String.valueOf(textInputEditTextReturnDate.getText());
+
+                if(!student_id.equals("") && !book_id.equals("") && !borrowdate.equals("") && !returndate.equals("")) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[4];
+                            field[0] = "student_id";
+                            field[1] = "book_id";
+                            field[2] = "borrowdate";
+                            field[3] = "returndate";
+                            String[] data = new String[4];
+                            data[0] = student_id;
+                            data[1] = book_id;
+                            data[2] = borrowdate;
+                            data[3] = returndate;
+                            PutData putData = new PutData("http://192.168.1.111/CA_database/booking_books.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Apply Complete")){
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Booking.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"All fields required", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- app/src/main/java/com/example/collegeappv02/booking_rooms.java
+++ app/src/main/java/com/example/collegeappv02/booking_rooms.java
@@ -4,12 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class booking_rooms extends AppCompatActivity {
 
-    Button button;
+    TextInputEditText textInputEditTextStudentID, textInputEditTextRoomID, textInputEditTextDate, textInputEditTextStartingTime, textInputEditTextEndingTime;
+    Button apply,button;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,70 @@ public class booking_rooms extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), Booking.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        textInputEditTextStudentID = findViewById(R.id.booking_rooms_student_id);
+        textInputEditTextRoomID = findViewById(R.id.booking_rooms_room_id);
+        textInputEditTextDate = findViewById(R.id.booking_rooms_date);
+        textInputEditTextStartingTime = findViewById(R.id.booking_rooms_startingtime);
+        textInputEditTextEndingTime = findViewById(R.id.booking_rooms_endingtime);
+        apply = findViewById(R.id.buttonApplyRooms);
+        progressBar = findViewById(R.id.progress);
+
+
+        apply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String student_id, rooms_id, date, startingtime, endingtime;
+                student_id = String.valueOf(textInputEditTextStudentID.getText());
+                rooms_id = String.valueOf(textInputEditTextRoomID.getText());
+                date = String.valueOf(textInputEditTextDate.getText());
+                startingtime = String.valueOf(textInputEditTextStartingTime.getText());
+                endingtime = String.valueOf(textInputEditTextEndingTime.getText());
+
+                if(!student_id.equals("") && !rooms_id.equals("") && !date.equals("") && !startingtime.equals("") && !endingtime.equals("")) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    Handler handler = new Handler();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] field = new String[5];
+                            field[0] = "student_id";
+                            field[1] = "rooms_id";
+                            field[2] = "date";
+                            field[3] = "startingtime";
+                            field[4] = "endingtime";
+                            String[] data = new String[5];
+                            data[0] = student_id;
+                            data[1] = rooms_id;
+                            data[2] = date;
+                            data[3] = startingtime;
+                            data[4] = endingtime;
+                            PutData putData = new PutData("http://192.168.1.111/CA_database/booking_rooms.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Apply Complete")){
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Booking.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"All fields required", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
