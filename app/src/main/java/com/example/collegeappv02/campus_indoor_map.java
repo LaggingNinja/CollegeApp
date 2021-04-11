4022a6f5788a593d2afb4e7975dc57c711afeb41
LaggingNinja <66457844+LaggingNinja@users.noreply.github.com>
2021-04-11 11:06:16 +0800

    (11/4/2021 NEWEST) -finished indoor map.

--- app/src/main/java/com/example/collegeappv02/campus_indoor_map.java
+++ app/src/main/java/com/example/collegeappv02/campus_indoor_map.java
@@ -2,7 +2,10 @@ package com.example.collegeappv02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,10 +13,25 @@ import java.util.ArrayList;
 
 public class campus_indoor_map extends AppCompatActivity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus_indoor_map);
+
+        button = findViewById(R.id.back);
+        //when click back
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), campus_indoor.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
 
     }
 
